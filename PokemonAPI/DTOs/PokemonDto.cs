c2997f74ef901e218759da5695bd110516ca24dd
Ausubel <ausubelvc@gmail.com>
2024-02-29 11:25:15 -0500

    cleaning PokemonDto

--- PokemonAPI/DTOs/PokemonDto.cs
+++ PokemonAPI/DTOs/PokemonDto.cs
@@ -12,20 +12,6 @@
         
         public static bool IsValid(PokemonDto pokemon)
         {
-            Console.WriteLine(pokemon.Name);
-            Console.WriteLine(pokemon.TypeId);
-            Console.WriteLine(pokemon.DietId);
-            Console.WriteLine(pokemon.SizeId);
-            Console.WriteLine(pokemon.WeightKg);
-            Console.WriteLine(pokemon.RarityId);
-            Console.WriteLine(pokemon.FunFact);
-            Console.WriteLine(!string.IsNullOrEmpty(pokemon.Name));
-            Console.WriteLine(pokemon.TypeId > 0);
-            Console.WriteLine(pokemon.DietId > 0);
-            Console.WriteLine(pokemon.SizeId > 0);
-            Console.WriteLine(!string.IsNullOrEmpty(pokemon.WeightKg));
-            Console.WriteLine(pokemon.RarityId > 0);
-            Console.WriteLine(!string.IsNullOrEmpty(pokemon.FunFact));
             return !string.IsNullOrEmpty(pokemon.Name) && pokemon.TypeId > 0 && pokemon.DietId > 0 && pokemon.SizeId > 0 && !string.IsNullOrEmpty(pokemon.WeightKg) && pokemon.RarityId > 0 && !string.IsNullOrEmpty(pokemon.FunFact);
         }
     }
